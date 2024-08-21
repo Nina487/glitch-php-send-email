2dd743e34ae70ea7b06d721fafcdfd7299142eb3
student <student@itl.lan>
2024-08-21 08:28:03 +0200

    added sending email

---
+++ public/contact.php
@@ -0,0 +1,39 @@
+<?php
+
+//These 3 lines implement PHPMailer library
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+    //Creating PHPMailer instance
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Getting destination mail, subject and mail body from index.php form
+    $mailTo = $_POST['mailTo'];
+    $subject = $_POST['subject'];
+    $body = $_POST['body'];
+
+    //Server settings
+    $mail->SMTPDebug = PHPMailer\PHPMailer\SMTP::DEBUG_SERVER; //To remove log messages replace PHPMailer\PHPMailer\SMTP::DEBUG_SERVER with number: 0               
+    $mail->isSMTP();  //Choosing SMTP server for sending messages                                         
+    $mail->Host       = 'smtp.gmail.com'; //Host for SMTP messages (gmail)               
+    $mail->SMTPAuth   = true; //SMTP needs authentication                                 
+    $mail->Username   = 'nina.lucna333@gmail.com'; //Email  you want to send FROM (gmail)             
+    $mail->Password   = 'lpne ozxd kpxc xuva';  // Low security application password for your google account, if you don't know how to generate it, just message me                           
+    $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS; //Encryption      
+    $mail->Port       = 587; //port
+
+    //Recipients
+    $mail->setFrom('nina.lucna333@gmail.com'); // Your email address also here
+    $mail->addAddress($mailTo); //Email you're sending TO, in this example specified in index.php form
+
+    $mail->isHTML(true); //Enabling HTML tags in email body
+    $mail->Subject = $subject; //Email subject from index.php form
+    $mail->Body    = $body; //Email body from index.php form
+
+    $mail->send(); //Sending email here
+    echo '<span style="color: green">Message has been sent</span>';
+} catch (Exception $e) { //If someting went wrong displaying error below
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
--- public/index.php
+++ public/index.php
@@ -1,1 +1,18 @@
-<?php phpinfo();
+<form method="post" action="contact.php">
+    <input type="text" name="mailTo" placeholder="destination email"><br><br>
+    <input type="text" name="subject" placeholder="Subject"><br><br>
+    <input type="text" name="body" placeholder="E-mail body"><br><br>
+    <button type="submit">Send</button>
+</form>
+
+<br>
+<br>
+You need a local webserver to be able to run PHP scripts (e.g XAMPP). If you don't have a local webserver I'll help you configure one.
+<br>
+This example is made to work with gmail (your own mail has to be gmail, destination can be anything else)
+<br>
+Everything important is commented in the contact.php file.
+<br>
+To get this work, you need to replace $mail->Username, $mail->Password, $mail->setFrom('youEmail@gmail.com') variables in contact.php with you own data.<br>
+For implementing this to your project, you need to copy the PHPMailer folder and put it in the same directory as the file where the email sending script is !!!IMPORTANT!!!<br><br>
+To implement php code to your website you can just try to make it identical like this example project. Two files: one with a form and a submit button, second file with sending mail script. Method="post" and action="contact.php"(your script filename) in the form are very important.
